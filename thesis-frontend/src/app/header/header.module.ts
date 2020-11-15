2085ed6cba0221351a5f9f2d1d25fe20d988314c
alexanderhodes <alexander.hodes@live.com>
2020-11-15 09:08:59 +0100

    added some links for navigation between pages

--- thesis-frontend/src/app/header/header.module.ts
+++ thesis-frontend/src/app/header/header.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header.component';
 import {CommonModule} from '@angular/common';
 import {SharedModule} from '../shared';
+import {RouterModule} from '@angular/router';
 
 const components = [
   HeaderComponent
@@ -14,10 +15,11 @@ const components = [
   exports: [
     ...components
   ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        RouterModule
+    ],
   providers: []
 })
 export class HeaderModule {
