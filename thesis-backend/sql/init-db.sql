28e3c349434718ca61de158d91f23b2d4f4cc38a
alexanderhodes <alexander.hodes@live.com>
2020-11-27 17:54:03 +0100

    initialize db script

--- thesis-backend/sql/init-db.sql
+++ thesis-backend/sql/init-db.sql
@@ -1,8 +1,17 @@
+-- clear everything
+delete from public.user_roles_role where 1 = 1;
+delete from public.user where 1 = 1;
+delete from public.role_permissions_permission where 1 = 1;
+delete from public.permission where 1 = 1;
+delete from public.role where 1 = 1;
+
+-- insert roles
 insert into public.role (name) values ('BOARD');
 insert into public.role (name) values ('INTERN');
 insert into public.role (name) values ('ADMINISTRATOR');
 insert into public.role (name) values ('EXTERN');
 
+-- insert permissions
 insert into public.permission (name) values ('ASSETS_CREATE');
 insert into public.permission (name) values ('ASSETS_READ');
 insert into public.permission (name) values ('ASSETS_UPDATE');
@@ -18,6 +27,7 @@ insert into public.permission (name) values ('CONFIGURATION_READ');
 insert into public.permission (name) values ('CONFIGURATION_UPDATE');
 insert into public.permission (name) values ('CONFIGURATION_DELETE');
 
+-- insert role and permissions
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('BOARD', 'ASSETS_CREATE');
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('BOARD', 'ASSETS_READ');
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('BOARD', 'ASSETS_UPDATE');
@@ -37,3 +47,9 @@ insert into public.role_permissions_permission ("roleName", "permissionName") va
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('ADMINISTRATOR','CONFIGURATION_READ') ;
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('ADMINISTRATOR','CONFIGURATION_UPDATE') ;
 insert into public.role_permissions_permission ("roleName", "permissionName") values ('ADMINISTRATOR','CONFIGURATION_DELETE') ;
+
+-- insert default user
+insert into public.user ("username", "password", "publicKey") values ('alex','$2b$10$RsMvon91WvCUcOH40q2l4.DAJHzChShQWRnSk2/nQ3.7tGBenph6W','GmqY981bJL6JFN82UrD421bcRxkwV3p2s97ZLf6smzkb');
+insert into public.user_roles_role ("userId", "roleName") values ('8a5ecfc8-7d3a-40cb-95e4-5cfb59e3f889','BOARD');
+insert into public.user_roles_role ("userId", "roleName") values ('8a5ecfc8-7d3a-40cb-95e4-5cfb59e3f889','ADMINISTRATOR');
+
