32b6fcbb3725942196b4d6ba1d1e83aaa9425c94
alexanderhodes <alexander.hodes@live.com>
2020-11-28 09:39:17 +0100

    removed commented lines

--- thesis-backend/src/app.module.ts
+++ thesis-backend/src/app.module.ts
@@ -1,40 +1,15 @@
 import {Module} from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
 import {ScheduleModule} from "@nestjs/schedule";
+import {TypeOrmModule} from '@nestjs/typeorm';
 import {TestModule} from './test';
 import {AuthorizationModule} from './authorization';
 import {BigchainModule} from './bigchain';
 import {ApiModule} from './api';
-import {TypeOrmModule} from '@nestjs/typeorm';
 import {AppConfigModule, ConfigurationService} from './app-config';
-
-// export function createTypeOrmConfigFactory(configService: ConfigurationService): TypeOrmModuleOptions {
-//     const dbConfig: any = {
-//         type: configService.get<string>('DATABASE_TYPE'),
-//         host: configService.get<string>('DATABASE_HOST'),
-//         port: Number(configService.get<string>('DATABASE_PORT')),
-//         username: configService.get<string>('DATABASE_USERNAME'),
-//         password: configService.get<string>('DATABASE_PASSWORD'),
-//         database: configService.get<string>('DATABASE_NAME')
-//     };
-//
-//     return {
-//         ...dbConfig,
-//         entities: [ Permission, User ],
-//         synchronize: true,
-//         migrationsTableName: 'migrations',
-//         migrations: [
-//             InsertPermissions1601456259497Migration
-//         ],
-//         cli: {
-//             migrationsDir: "src/database/migrations"
-//         }
-//     }
-// }
 
 @Module({
     imports: [
-//        MongooseModule.forRoot("mongodb://localhost:27018/bigchain"),
         MongooseModule.forRootAsync({
            imports: [AppConfigModule],
            inject: [ConfigurationService],
@@ -45,7 +20,6 @@ import {AppConfigModule, ConfigurationService} from './app-config';
             imports: [AppConfigModule],
             inject: [ConfigurationService],
             useFactory: (configurationService: ConfigurationService) => configurationService.createTypeOrmConfigFactory()
-//            useFactory: createTypeOrmConfigFactory
         }),
         AuthorizationModule,
         TestModule,
--- thesis-backend/src/test/controllers/app.controller.ts
+++ thesis-backend/src/test/controllers/app.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from '../services';
-import {ConfigurationService} from '../../app-config/services';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from '../services';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-              private configurationService: ConfigurationService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const value = this.configurationService.get('MONGODB_PATH');
-    const xyz = this.configurationService.get('BAZINGA');
-    console.log('value', value, xyz);
     return this.appService.getHello();
   }
 }
