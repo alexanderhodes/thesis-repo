fd276e42b7e9530f5e8e9827306459143570fa0c
alexanderhodes <alexander.hodes@live.com>
2020-11-29 12:39:06 +0100

    updated build depending on environment

--- thesis-backend/scripts/config.ts
+++ thesis-backend/scripts/config.ts
@@ -15,7 +15,7 @@ interface Config {
     }
 }
 
-function createConfig(): void {
+function createConfig(command: string, isProduction: boolean): void {
     // check if os is windows and exit if yes
     const isWindows = process.platform === 'win32';
     if (isWindows) {
@@ -36,8 +36,12 @@ function createConfig(): void {
         if (configFile) {
             const config: Config = JSON.parse(configFile);
             console.log('config', config);
-            exec('hostname -f', (err, stdout) => {
+            exec(command, (err, stdout) => {
                 if (!err) {
+                    if (!isProduction) {
+                        stdout = stdout.split(' ')[0];
+                        console.log('updated ip address', stdout);
+                    }
                     const hostname = stdout.replace('\n', '');
                     console.log('hostname', hostname);
                     if (hostname) {
@@ -55,4 +59,20 @@ function createConfig(): void {
     }
 }
 
-createConfig();
+const isProd = process.argv.indexOf('--prod') > -1;
+const isLocal = process.argv.indexOf('--local') > -1;
+
+if (isLocal) {
+    // for local virtual machine -> use ip address instead of hostname
+    console.log('create local config for local virtual machine by using ip address');
+    const command = 'hostname -I';
+    createConfig(command, false);
+} else if (isProd) {
+    // for virtual machine -> use full hostname
+    console.log('create prod config by using full hostname');
+    const command = 'hostname -f';
+    createConfig(command, true);
+} else {
+    // for development environment
+    console.log('nothing to do with dev config');
+}
