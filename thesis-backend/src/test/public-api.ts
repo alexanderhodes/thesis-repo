2a7b648b1dc335bb5034f598a3ac4620400e172b
alexanderhodes <alexander.hodes@live.com>
2020-12-07 16:40:41 +0100

    removed unnecessary code

--- thesis-backend/src/test/public-api.ts
+++ thesis-backend/src/test/public-api.ts
@@ -1,4 +1,2 @@
 export * from './test.module';
 export * from './scheduler';
-export * from './schemas';
-export * from './services';
--- thesis-backend/src/test/test.module.ts
+++ thesis-backend/src/test/test.module.ts
@@ -1,20 +1,12 @@
 import {Module} from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose';
-import {ColService} from './services';
 import {TasksService} from './scheduler';
-import {Col, ColSchema} from './schemas';
-import {AppConfigModule} from '../app-config';
 
 const services = [
-    ColService,
     TasksService
 ];
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{name: Col.name, schema: ColSchema}]),
-        AppConfigModule
-    ],
+    imports: [],
     controllers: [],
     providers: [
         ...services
