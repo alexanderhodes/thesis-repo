b435ee96962d93cb1585ee502e2643e50eb1454d
alexanderhodes <alexander.hodes@live.com>
2020-12-10 14:14:51 +0100

    breadcrumb code optimization

--- thesis-frontend/src/app/core/components/breadcrumb/breadcrumb.component.ts
+++ thesis-frontend/src/app/core/components/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,6 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Observable} from 'rxjs';
-import {map, takeUntil} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {BreadcrumbService} from '../../services';
 import {CleanUpHelper} from '../../utils';
 import {IBreadcrumb} from '../../interfaces';
@@ -14,25 +14,16 @@ import {IBreadcrumb} from '../../interfaces';
 })
 export class BreadcrumbComponent extends CleanUpHelper implements OnInit {
 
-  show: boolean;
-  breadcrumbs: IBreadcrumb[];
+  show$: Observable<boolean>;
+  breadcrumbs$: Observable<IBreadcrumb[]>;
 
-  constructor(private breadcrumbService: BreadcrumbService,
-              private changeDetectorRef: ChangeDetectorRef) {
+  constructor(private breadcrumbService: BreadcrumbService) {
     super();
   }
 
   ngOnInit(): void {
-    this.breadcrumbService.isShown$().pipe(takeUntil(this.onDestroy$)).subscribe(show => {
-      console.log('show', show);
-      this.show = show;
-      this.changeDetectorRef.detectChanges();
-    });
-    this.breadcrumbService.getBreadcrumb$().pipe(takeUntil(this.onDestroy$)).subscribe(breadcrumbs => {
-      this.breadcrumbs = breadcrumbs;
-      console.log('breadcrumbs', this.breadcrumbs);
-      this.changeDetectorRef.detectChanges();
-    });
+    this.show$ = this.breadcrumbService.isShown$().pipe(takeUntil(this.onDestroy$));
+    this.breadcrumbs$ = this.breadcrumbService.getBreadcrumb$().pipe(takeUntil(this.onDestroy$));
   }
 
 }
