ec2b011cae7cfb2c96096c78123a8f8143bdde98
alexanderhodes <alexander.hodes@live.com>
2020-12-12 13:05:41 +0100

    added start scripts for linux

---
+++ scripts/clean-start-container.sh
@@ -0,0 +1,20 @@
+# remove docker containers
+sudo docker container rm bigchaindb
+sudo docker container rm neo4j
+
+# start bigchain db with docker
+sudo docker run \
+  --detach \
+  --name bigchaindb \
+  --net=host \
+  --volume $HOME/bigchaindb_docker/mongodb/data/db:/data/db \
+  --volume $HOME/bigchaindb_docker/mongodb/data/configdb:/data/configdb \
+  --volume $HOME/bigchaindb_docker/tendermint:/tendermint \
+  bigchaindb/bigchaindb:all-in-one
+# start neo4j with docker
+sudo docker run \
+  --detach \
+  --name neo4j \
+  --net=host \
+  --env NEO4J_AUTH=neo4j/j4oen \
+  neo4j:latest
---
+++ scripts/start-container.sh
@@ -0,0 +1,6 @@
+# start bigchain db with docker
+sudo docker container start bigchaindb
+# start neo4j with docker
+sudo docker container start neo4j
+# list containers
+sudo docker container list
